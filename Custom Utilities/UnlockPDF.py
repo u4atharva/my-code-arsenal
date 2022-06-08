3fa7d038c19b56ee4f2a341dc6ade23f13c4ca38
Atharva Gupta <62495214+u4atharva@users.noreply.github.com>
2022-06-08 15:08:09 +0530

    Update UnlockPDF.py

--- Custom Utilities/UnlockPDF.py
+++ Custom Utilities/UnlockPDF.py
@@ -4,4 +4,5 @@ import os
 for filename in os.listdir('NMIMS copy'):
     if filename.__contains__('.pdf'):
         pdf = pikepdf.open(r"NMIMS copy/" + str(filename))
+        # pdf = pikepdf.open(r"NMIMS copy/" + str(filename), password = '<password here>')
         pdf.save('Extracted/' + str(filename))
